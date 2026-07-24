b940f9c37cfc48f23dd8433ea51ddbf62c970ab5
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-07-24 12:27:21 -0400

    improve the experience of using the simulator

--- src/simulator/src/simulator/main.py
+++ src/simulator/src/simulator/main.py
@@ -1,10 +1,9 @@
 import argparse
 import pathlib
 
-# TODO: this works. but we can do better. 
 from simulator.runner import run_simulation
 
-# TODO: rewrite parallel.py to take in the argparse arguments.
+# TODO: port parallel.py to fit in this new shape.
 
 def main():    
     parser = argparse.ArgumentParser(
@@ -13,16 +12,26 @@ def main():
         epilog="Copyright Nathaniel Flores 2026, MIT license."
     )
     sub = parser.add_subparsers(dest="command", required=True)
+    base = pathlib.Path().resolve()
 
     run_p = sub.add_parser("run",
-        description="Simulate sea ice buoys",
+        description="Simulate Lagrangian buoys in sea ice",
         epilog="This can be very computationally intensive.")
     run_p.add_argument("-v", "--verbose",
                        help="enable debug messages",
                        action="store_true")
-    run_p.add_argument("-d", "--directory",
-                       help="output to a specific directory",
-                       action="store", type=pathlib.Path)
+    run_p.add_argument("-o", "--out-dir", metavar="PATH",
+                       help="default: ./sim-data/runs",
+                       action="store", type=pathlib.Path,
+                       default=base / "sim-data" / "runs")
+    run_p.add_argument("-b", "--buoy-data", metavar="PATH",
+                       help="default: ./buoy-data",
+                       action="store", type=pathlib.Path,
+                       default=base / "buoy-data")
+    run_p.add_argument("-i", "--ice-data", metavar="PATH",
+                       help="default: ./ice-data",
+                       action="store", type=pathlib.Path,
+                       default=base / "ice-data")
     run_p.set_defaults(func=run_simulation)
 
     plot_p = sub.add_parser("plot",
@@ -31,9 +40,14 @@ def main():
     plot_p.add_argument("-v", "--verbose",
                        help="enable debug messages",
                        action="store_true")
-    plot_p.add_argument("-d", "--directory",
-                       help="output to a specific directory",
-                       action="store", type=pathlib.Path)
+    plot_p.add_argument("-i", "--in-dir", metavar="PATH",
+                       help="default: ./sim-data/runs",
+                       action="store", type=pathlib.Path,
+                       default=base / "sim-data" / "runs")
+    plot_p.add_argument("-o", "--out-dir", metavar="PATH",
+                       help="default: ./sim-data/plots",
+                       action="store", type=pathlib.Path,
+                       default=base / "sim-data" / "plots")
 
     fetch_p = sub.add_parser("fetch",
         description="Download required data for the simulator",
@@ -41,9 +55,14 @@ def main():
     fetch_p.add_argument("-v", "--verbose",
                        help="enable debug messages",
                        action="store_true")
-    fetch_p.add_argument("-d", "--directory",
-                       help="output to a specific directory",
-                       action="store", type=pathlib.Path)
+    fetch_p.add_argument("-b", "--buoy-data", metavar="PATH",
+                       help="default: ./buoy-data",
+                       action="store", type=pathlib.Path,
+                       default=base / "buoy-data")
+    fetch_p.add_argument("-i", "--ice-data", metavar="PATH",
+                       help="default: ./ice-data",
+                       action="store", type=pathlib.Path,
+                       default=base / "ice-data")
     
     args = parser.parse_args()
     args.func(args)
--- src/simulator/src/simulator/runner.py
+++ src/simulator/src/simulator/runner.py
@@ -1,20 +1,71 @@
-# TODO: figure out how to import simlib in here
-from dataclasses import dataclass
-from pathlib import Path
+import pathlib
+import time
+import os
+import multiprocessing
 
-from simlib.tools.logging import conf_interactive_logger, LogLevel
+from functools import partial
+from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor
 
-@dataclass()
-class SimulatorPrefs:
-    out_path: Path
+from simlib.tools.logging import (
+    LogLevel,
+    conf_interactive_logger,
+    conf_manager_logger,
+    conf_worker_logger
+)
+from simlib.core import IceTrajectory, BuoyTrajectory, Simulator
+
+# TODO: redefine the workers when real workload time
+def simulator_worker(
+        item: tuple[int, int],
+        log_queue: multiprocessing.Queue
+    ):
+    key, obj = item
+    log = conf_worker_logger(str(key),
+                             log_queue,
+                             LogLevel.DEBUG)
+    log.debug(f"hello! my number is: {str(obj)}")
+    return (key, obj)
+
+def create_simpaths(outdir: pathlib.Path) -> pathlib.Path:
+    """Create the user-specified paths."""
+    pathname = "run_" + datetime.now().strftime("%Y-%m-%d_%H:%M")
+    newpath = outdir / pathname
+    os.makedirs(newpath)
+    return newpath
 
 def run_simulation(args):
-    log = conf_interactive_logger("main", LogLevel.INFO)
-    print("""
-    They say science is done on the shoulders of giants.
-    Not here; at Aperture we do all our science from scratch. No hand-holding. 
-    """)
-    if args.verbose:
-        print("And we're also very verbose.")
-    print(args.directory)
-    log.debug("we are LIVE")
+    mainlog = conf_interactive_logger("main", LogLevel.DEBUG)
+    init_time = time.perf_counter()
+    mainlog.info("Preparing simulation for runtime...")
+    mainlog.info(f"Working path: {pathlib.Path().resolve()}")
+    queue = multiprocessing.Manager().Queue()
+    output = create_simpaths(args.out_dir)
+    mainlog.info(f"Saving to: {output}")
+    listener = conf_manager_logger(queue, output / "workers.log")
+    listener.start()
+    # map can only take one argument, so we need to curry here
+    partial_worker = partial(simulator_worker, log_queue=queue)
+    mainlog.info("Preparations complete. Executing...")
+
+    # TODO: replace this example with the real one.
+    randomstuff = dict()
+    for i in range(0, 100000):
+        randomstuff[i] = i * 2
+    
+    # the default is not fine because I/O overhead is dominated by context switch,
+    # so we actually want to keep the pool overscheduled.
+    with ProcessPoolExecutor() as ex:
+        results = dict(
+            ex.map(
+                partial_worker,
+                randomstuff.items(),
+                chunksize=1
+            )
+        )
+
+    listener.stop()
+    mainlog.info(f"Simulation complete. Processed {len(results.keys())} items.")
+    
+    end_time = time.perf_counter()
+    mainlog.info(f"Elapsed: { end_time - init_time:.2f}")
