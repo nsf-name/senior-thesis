f224aca57c8c235d392fce9296bca9caaf2d1999
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-07-27 10:10:30 -0400

    improve logging in the base model

--- src/simlib/src/simlib/core/basemodel.py
+++ src/simlib/src/simlib/core/basemodel.py
@@ -4,7 +4,9 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Optional
 
-import numpy as np
+from logging import Logger
+
+import polars as pl
 from simlib.tools.logging import LogLevel, conf_interactive_logger
 from simlib.tools.utilities import DataExportType, UnfinishedSimulatorError
 
@@ -21,6 +23,7 @@ class Trajectory(ABC):
         date_list: Timestamps corresponding to each entry in pos_list. For external capture.
         dist_list: Haversine distances between positions. For external capture.
         loglevel: The amount of logs to be emitted. If None, uses LogLevel.WARNING.
+        logger: Log output. If None, creates one for interactive use.
         timestep: Duration between simulation steps. If None, iterates through everything.
     """
 
@@ -31,19 +34,21 @@ class Trajectory(ABC):
     date_list: list[datetime] = field(default_factory=list)
     dist_list: list[float] = field(default_factory=list)
     loglevel: Optional[LogLevel] = None
+    logger: Optional[Logger] = None
     timestep: Optional[timedelta] = None
 
     def __post_init__(self):
-        # TODO: fix timestamp
         self._t = self.start_day
         self._is_consumed = False
-        self._pos = np.zeros(2, dtype=np.float64)
 
         self._filename = f"{self.id}-{type(self).__name__}"
-        if self.loglevel is not None:
+        if self.loglevel is None:
+            self.loglevel = LogLevel.WARNING
+
+        if self.logger is None:
             self._log = conf_interactive_logger(self._filename, self.loglevel)
         else:
-            self._log = conf_interactive_logger(self._filename, LogLevel.WARNING)
+            self._log = self.logger
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}[{self.id}](consumed: {self.consumed})"
@@ -53,47 +58,22 @@ class Trajectory(ABC):
     @abstractmethod
     def __len__(self) -> int: ...
 
-    def __iter__(self):
-        return self
-
-    @abstractmethod
-    def __next__(self) -> np.ndarray: ...
-
-    def step(self):
-        """Step the simulator by exactly one tick."""
-        return next(self)
-
     def export(self, type: DataExportType, path: Path):
         """Export the simulator data to the specified type."""
         match type:
-            case DataExportType.GEOJSON:
-                xs, ys = zip(*self.poslist)
-                record = dict(x=xs, y=ys)
-                try:
-                    import geopandas as gpd
-
-                    gpd.GeoDataFrame(
-                        # for more types of data, add more entries to this dict
-                        {
-                            "geometry": gpd.gpd.points_from_xy(
-                                record["x"], record["y"], crs="EPSG:4326"
-                            )
-                        }
-                    ).to_file(path / f"{self._filename}.geojson", driver="GeoJSON")
-                except Exception as err:
-                    self._log.exception(f"unexpected {err=} on export")
             case DataExportType.CSV:
-                # timestamp list and the position list are always the same length
-                zipper = [
-                    (*pos, t, self.id) for pos, t in zip(self.poslist, self.timelist)
-                ]
+                lats, lons = zip(*self.poslist)
                 try:
-                    import pandas as pd
-
-                    with open(path / self._filename, "w") as f:
-                        pd.DataFrame(
-                            zipper, columns=["lat", "lon", "time", "id"]
-                        ).to_csv(f)
+                    # TODO: export distlist
+                    pl.DataFrame(
+                        {
+                            "lat": list(lats),
+                            "lon": list(lons),
+                            "time": self.timelist,
+                            "id": self.id,
+                        },
+                        orient="row",
+                    ).write_csv(path)
                 except Exception as err:
                     self._log.exception(f"unexpected {err=} on export")
             case DataExportType.NETCDF:
@@ -105,6 +85,10 @@ class Trajectory(ABC):
     @property
     def consumed(self) -> bool:
         return self._is_consumed
+
+    @property
+    def filename(self) -> str:
+        return self._filename
 
     @property
     def poslist(self) -> list[tuple[float, float]]:
@@ -128,39 +112,29 @@ class Trajectory(ABC):
         return self.dist_list
 
     @property
-    def start_time(self) -> datetime:
-        """Returns the start time if defined; otherwise, returns first day in the data."""
-        if self.start_day is None:
-            if len(self.timelist) >= 2:
-                return self.timelist[0]
-            else:
-                raise UnfinishedSimulatorError("Simulator must be consumed first")
-        else:
-            return self.start_day
-
-    @property
-    def end_time(self) -> datetime:
-        """Returns the end time if defined; otherwise, returns last day in the data."""
-        if self.end_day is None:
-            if len(self.timelist) >= 2:
-                return self.timelist[-1]
-            else:
-                raise UnfinishedSimulatorError("Simulator must be consumed first")
-        else:
-            return self.end_day
-
-    @property
-    def start_pos(self) -> tuple[float, float]:
-        """Returns the first position from the simulation data safely."""
-        if len(self.poslist) >= 2:
-            return self.poslist[0]
-        else:
+    def poshead(self) -> tuple[float, float]:
+        """Peek the first value of the poslist."""
+        if not self.consumed:
             raise UnfinishedSimulatorError("Simulator must be consumed first")
+        return self.pos_list[0]
 
     @property
-    def end_pos(self) -> tuple[float, float]:
-        """Returns the first position from the simulation data safely."""
-        if len(self.poslist) >= 2:
-            return self.poslist[-1]
-        else:
+    def postail(self) -> tuple[float, float]:
+        """Peek the last value of the poslist."""
+        if not self.consumed:
             raise UnfinishedSimulatorError("Simulator must be consumed first")
+        return self.pos_list[-1]
+
+    @property
+    def timehead(self) -> datetime:
+        """Peek the first value of the timelist."""
+        if not self.consumed:
+            raise UnfinishedSimulatorError("Simulator must be consumed first")
+        return self.date_list[0]
+
+    @property
+    def timetail(self) -> datetime:
+        """Peek the last value of the timelist."""
+        if not self.consumed:
+            raise UnfinishedSimulatorError("Simulator must be consumed first")
+        return self.date_list[-1]
