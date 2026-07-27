fc768d29b9537862ab69cc3199ab1a63789c1416
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-07-27 10:09:44 -0400

    cut down on data loader methods since they're no longer needed

--- src/simlib/src/simlib/core/__init__.py
+++ src/simlib/src/simlib/core/__init__.py
@@ -1,5 +1,5 @@
 from .basemodel import Trajectory
 from .buoymodel import BuoyTrajectory
 from .icemodel import IceTrajectory
-from .dataloader import load_buoy_data, load_ice_data
+from .dataloader import load_ice_data
 from .simulator import Simulator
--- src/simlib/src/simlib/core/dataloader.py
+++ src/simlib/src/simlib/core/dataloader.py
@@ -3,35 +3,15 @@ from glob import glob
 
 import xarray as xr
 
-# TODO: make it reproducible. this is all so grotesquely specific to my machine...
-
-class SimDataType(Enum):
-    """Valid simulation data types."""
-    BUOY = auto()
-    ICE = auto()
-
-def load_buoy_data() -> xr.Dataset:
-    xds = xr.open_dataset(
-        "/Users/nsf/Documents/Code/senior-thesis/src/buoy.nc",
-        engine="h5netcdf"
-    )
-    xds = xds.set_index(index=['BuoyID', 'time'])
-    return xds
 
 def load_ice_data() -> xr.Dataset:
-  """Sets up the ice data."""
-  xds = xr.open_mfdataset(
-    sorted(glob("/Users/nsf/Documents/Datasets/icemotion/*.nc")),
-    data_vars="all",
-    combine="by_coords",
-  )
-  # non-standard calendar warning off, plus literally who cares about precision, we're working in days and weeks here
-  xds["time"] = xds.indexes["time"].to_datetimeindex(unsafe=True, time_unit='ns')
-  return xds
-
-def load_grid_data() -> xr.Dataset:
-    xds = xr.open_dataset(
-        "/Users/nsf/Documents/Code/senior-thesis/src/NSIDC0772_LatLon_EASE_N25km_v1.1.nc",
-        engine="h5netcdf"
+    """Sets up the ice data."""
+    xds = xr.open_mfdataset(
+        sorted(glob("/Users/nsf/Documents/Datasets/icemotion/*.nc")),
+        data_vars="all",
+        combine="by_coords",
     )
+    # correct from Julian to Gregorian to make the math easier
+    # we're ignoring all the errors because they're very small relatively
+    xds["time"] = xds.indexes["time"].to_datetimeindex(unsafe=True, time_unit="ns")
     return xds
